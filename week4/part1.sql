51772ffa61ad519ad31068b62eaab6f8a01eb897
Salamatbek <73515656+Sakubek1337@users.noreply.github.com>
2022-06-16 18:21:18 +0600

    Week-4 completed.
    yes.

---
+++ week4/part1.sql
@@ -0,0 +1,25 @@
+CREATE TABLE countries(
+		id int PRIMARY KEY, 
+		name varchar(255) NOT NULL
+    );
+CREATE TABLE brands(
+		id int PRIMARY KEY,
+		name varchar(255) NOT NULL
+    );
+CREATE TABLE warehouses(
+		id int PRIMARY KEY,
+		address varchar(255)
+    );
+CREATE TABLE employees(
+		id int PRIMARY KEY,
+		first_name varchar(255) NOT NULL,
+		last_name varchar(255) NOT NULL,
+		date_of_birth date NOT NULL,
+		salary_in_usd int
+	);
+CREATE TABLE phones(
+		id int PRIMARY KEY,
+        model varchar(255) NOT NULL,
+        year_made int NOT NULL,
+        price_in_usd int NOT NULL
+    );
